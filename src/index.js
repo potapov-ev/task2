1f133718d24a2a88dca9fc90ff2c289c36fbe5bc
mutik0137 <vsevolod121199@mail.ru>
2019-10-22 16:31:12 +0400

    r

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 
-// НАписать про глобал, заменить '
 ReactDOM.render(
   <App /> 
   , document.getElementById('root')
 );
-
-serviceWorker.unregister();
