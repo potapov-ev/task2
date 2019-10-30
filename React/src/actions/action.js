b5a9421fa4c5e916d839f8959c722a1c73f1b44d
mutik0137 <vsevolod121199@mail.ru>
2019-10-30 16:14:05 +0400

    r

--- React/src/actions/action.js
+++ React/src/actions/action.js
@@ -3,19 +3,27 @@ import Strapi from "strapi-sdk-javascript/build/main";
 
 export function itemsGetData(url) {
   return async (dispatch, getState) => {
-    if(getState().fetchItems.loading === false) { 
-      return;
-    }   
-
+    
     const loading = false;
     try {
       const strapi = new Strapi("http://localhost:1337");
       const items = await strapi.getEntries(url);
-      let numbers = new Array(items.length); // Кол-во единиц одного товара в корзине
-      for (let i = 0; i < numbers.length; ++i) {
-        numbers[i] = 1;
+
+      if (getState().numbers.length) {
+        if (getState().numbers.length === items.length) { 
+          return;
+        } else {
+          dispatch({type: "SET_NUMBERS", value: [...getState().numbers, 1]});
+        }
+      } else {
+        let numbers = new Array(items.length); // Кол-во единиц одного товара в корзине
+        for (let i = 0; i < numbers.length; ++i) {
+          numbers[i] = 1;
+        }
+        dispatch({type: "SET_NUMBERS", value: numbers});
       }
-      dispatch({type: "SET_NUMBERS", value: numbers});
+
+      
       const error = null;
       dispatch({type: "FETCH_ITEMS", value: {loading, error, items}});
     } catch (exception) {
